d3475d3a29b66f1e0db4b0c2d5664269939d2f27
AdrianSote <adrianmvparedes@gmail.com>
2024-06-28 13:19:30 +0200

    Gestion solucionada

--- PlenaInclusion/FrontClient/src/environments/environments.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environments = {
-    baseUrl: 'http://localhost:3000',
-    defautlProfileImage: "1fd5abd7-0784-41b7-814e-0755560d50c0",
-    noActivityImage: "7b60d078-068f-4856-9515-510a43116d7a",
-    plenaInlcusionLogo: "a835bda1-a96b-43e3-948f-cbb16cca6ef5",
-    client_id: '562380291200-abf1rofqn5kjjdlohfj45nhro3obdl7c.apps.googleusercontent.com',
-    plenaInclusionToast: '24831c35-b500-4751-9191-1ab19904d601'
-}
